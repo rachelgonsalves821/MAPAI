eacb68fbeb942564d87e57305f7d0afb7bba1ce1
rachelgonsalves821 <rachelgonsalves821@gmail.com>
2026-04-19 20:53:05 -0400

    fix: add babel module-resolver for @ path alias

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { reanimated: false }]],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
   };
 };
